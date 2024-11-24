9b44dfe18baedad0342c39023c16960d7fe2fbdf
luka onik <luka.onikadze@toptal.com>
2024-11-24 17:01:56 +0400

    refactor: demo updates

--- projects/demo/src/app/pages/element-y/element-y.component.ts
+++ projects/demo/src/app/pages/element-y/element-y.component.ts
@@ -3,7 +3,7 @@ import { UserCardComponent } from '../user-card/user-card.component';
 import { DataService } from '../../data.service';
 import { UserModel } from '../../models';
 import { BasePage } from '../base-page';
-import { InfScroll } from 'ng-inf-scroll'
+import { InfScroll } from 'ng-inf-scroll';
 
 @Component({
   selector: 'app-element-y',
@@ -13,31 +13,3 @@ import { InfScroll } from 'ng-inf-scroll'
   styleUrl: './element-y.component.scss',
 })
 export class ElementYComponent extends BasePage {}
-
-@Component({
-  selector: 'some-page',
-  styles: [
-    `
-      .container {
-        height: '200px';
-        overflow-y:auto;
-      }
-    `,
-  ],
-  template: `
-    <div class="container" infScroll (scrolled)="loadMore()">
-      @for (item of data; track $index) {
-      <h1>{{ item }}</h1>
-      }
-    </div>
-  `,
-  standalone: true,
-  imports: [InfScroll],
-})
-export class SomePageComponent {
-  data = new Array(100).fill(() => Math.random()); // Random numbers length:100
-
-  loadMore() {
-    // Load data
-  }
-}
